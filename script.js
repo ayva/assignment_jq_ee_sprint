4b0429a218b172c76ef4f6cc3e82e2582563da3f
ayva <olga.ayva@gmail.com>
2015-08-26 10:14:38 -0700

    more fields and refactoring

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-var counter = function(str){
-  return (32 - str.length);
-}
-var numberRemain = function() {
-  $('#info-about-text').text(counter($('#textfield').val()))
+//Count amount of characters left
+var counter = function(str,limit){
+  return (limit - str.length);
 }
 
-$('#textfield').keyup(numberRemain)
+//el represent this el we called keyup on
+var numberRemain = function(limit, el) {
+  $(el).next().text(counter($(el).val(),limit))
+}
+
+//Add p after all inputs
+$('input').after('<p></p>')
+
+$('#textfield').keyup(function() {
+  numberRemain(32, this)
+})
+
+$('input[type=password]').keyup(function() {
+  numberRemain(16, this)
+})
 
 
 
