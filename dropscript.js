cbf11e0bd574dc97b2077ec3ed8eaac06bc9a506
alokp <alokpra@gmail.com>
2015-08-26 13:56:31 -0700

    working on dropdown

--- dropscript.js
+++ dropscript.js
@@ -1,25 +1,30 @@
+
+$('#dropdown').
+
+
+
+
+
+// $( 'select' ).hover(
+
+//     function(){
+//       event.preventDefault();
+//       console.log('CONSOLE LOG');
+//       console.log($(this));
+
+//       $(this).trigger("click");
+//     }
+
+// );
+
+// $('option').hover(
+//   function(){
+//     event.preventDefault();
+//     $(this).css("background-color","green")
+//   })
+
 //$('select').click().show(1500);
 // $( 'select' ).click(function(){
 //   console.log($(this));
 //   $(this).after("WOW")
 // });
-
-$('#accordian').
-
-$( 'select' ).hover(
-
-    function(){
-      event.preventDefault();
-      console.log('CONSOLE LOG');
-      console.log($(this));
-   
-      $(this).trigger("click");
-    }
-
-);
-
-$('option').hover(
-  function(){
-    event.preventDefault();
-    $(this).css("background-color","green")
-  })
