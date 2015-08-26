5e49834750500b2eaccae093a279f31c85c28293
ayva <olga.ayva@gmail.com>
2015-08-26 15:38:13 -0700

    mouse box moves

--- photo.js
+++ photo.js
@@ -1,18 +1,32 @@
-$('img').mouseenter(function(){
-  $('img').after('<div id="box"></div>');
-  $(this).mousemove(function(event){
-    console.log(event.clientX, event.clientY);
-    $('#box').css({
-      'left': event.clientX,
-      'top': event.clientY,
+
+function moveCallback(event){
+     box();
+  }
+
+function box(){
+      $('#box').css({
+      'left': event.clientX-50,
+      'top': event.clientY-50,
       'height': '100px',
       'width': '100px',
       'border': '6px solid white',
       'position' : 'absolute'
     });
-  });
-});
+  }
 
-$('img').mouseleave(function(){
+function enterCallback(){
+  $('img').after('<div id="box"></div>');
+  $(this).mousemove( moveCallback );
+}
+
+$('#image-container').mouseenter(enterCallback);
+
+$('#image-container').click(function(){
+    $(this).off('mousemove', moveCallback).off('mouseenter', enterCallback);
+
+  });
+
+$('#image-container').mouseleave(function(){
   $('#box').remove();
 });
+
