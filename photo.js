783328715ae3194f90030b49d7845aff847bc7b1
alokp <alokpra@gmail.com>
2015-08-26 16:13:15 -0700

    working on dropdown

--- photo.js
+++ photo.js
@@ -1,18 +1,17 @@
-
 function moveCallback(event){
      box();
-  }
+}
 
 function box(){
-      $('#box').css({
-      'left': event.clientX-50,
-      'top': event.clientY-50,
-      'height': '100px',
-      'width': '100px',
-      'border': '6px solid white',
-      'position' : 'absolute'
-    });
-  }
+  $('#box').css({
+    'left': event.clientX-50,
+    'top': event.clientY-50,
+    'height': '100px',
+    'width': '100px',
+    'border': '6px solid white',
+    'position' : 'absolute'
+  });
+}
 
 function enterCallback(){
   $('img').after('<div id="box"></div>');
@@ -24,9 +23,37 @@ $('#image-container').mouseenter(enterCallback);
 $('#image-container').click(function(){
     $(this).off('mousemove', moveCallback).off('mouseenter', enterCallback);
 
-  });
-
-$('#image-container').mouseleave(function(){
-  $('#box').remove();
+    if ($('#person-selector').length === 0) {
+      $('#box').after('<ul id="person-selector"><li>Reports</li><li>Search</li><li>Graphs</li></ul>');
+      // $('#box').css({'height': '150px'});
+      $('#person-selector').css({ 'width': '100px',
+                                  'background-color': 'white',
+                                  'position' : 'absolute',
+                                  'left': event.clientX-50,
+                                  'bottom' : event.clientY-50
+                                });
+    }
 });
+
+// $('#image-container').mouseleave(function(){
+//   $('#box').remove();
+//   $('#person-selector').remove();
+// });
+
+$('#person-selector').children().mouseenter(function(){
+  $(this).css("background-color","green");
+  console.log(this);
+  $(this).click(function(){
+    $(this).siblings().remove();
+  });
+}).mouseleave(function(){
+  $(this).css("background-color","white");
+});
+
+$('select').hover(function(){
+    var count = $(this).children().length;
+    $(this).attr('size', count);
+     $(this).slideUp(3000);
+});
+
 
